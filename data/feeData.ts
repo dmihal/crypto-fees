5fb74f4df6cd7d9ed39398505c740a9a66fc39b1
David Mihal <david@dmihal.com>
2020-08-11 14:09:38 +0100

    Remove console logs

--- data/feeData.ts
+++ data/feeData.ts
@@ -15,7 +15,6 @@ export async function getFeeData(id: string): Promise<FeeData> {
   const startDateString = `${startDate.getFullYear()}-${(startDate.getMonth() + 1).toString().padStart(2, '0')}-${startDate.getDate().toString().padStart(2, '0')}`;
   const request = await fetch(`https://community-api.coinmetrics.io/v2/assets/${id}/metricdata?metrics=FeeTotUSD&start=${startDateString}`);
   const { metricData } = await request.json();
-  console.log(metricData);
   const sevenDayMA = metricData.series.reduce((total: number, value: any) => total + parseFloat(value.values[0]), 0) / metricData.series.length;
 
   return {
@@ -75,7 +74,6 @@ export async function getUniswapV1Data(): Promise<FeeData> {
     "method": "POST",
   });
   const { data } = await request.json();
-  console.log(data);
 
   const sevenDayMA = data.uniswapDayDatas.reduce((total: number, { dailyVolumeInUSD }: any) => total + parseFloat(dailyVolumeInUSD), 0) * 0.003 / data.uniswapDayDatas.length;
 
