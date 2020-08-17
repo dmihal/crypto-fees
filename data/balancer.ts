5ca3ae85eb00b166a3522845c75d14ff51581f34
David Mihal <david@dmihal.com>
2020-08-17 19:05:06 +0100

    ts-ignore

--- data/balancer.ts
+++ data/balancer.ts
@@ -3,6 +3,7 @@ import { FeeData } from './feeData';
 function dayOfYear(): number {
   const now = new Date();
   const start = new Date(now.getFullYear(), 0, 0);
+  // @ts-ignore
   const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
   const oneDay = 1000 * 60 * 60 * 24;
   const day = Math.floor(diff / oneDay);
