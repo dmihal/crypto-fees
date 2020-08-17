69695285f8cc9602e79b7fdbcb513d15585da8a2
David Mihal <david@dmihal.com>
2020-08-17 18:54:39 +0100

    Use new balancer subgraph to get data

--- data/balancer.ts
+++ data/balancer.ts
@@ -1,48 +1,50 @@
 import { FeeData } from './feeData';
 
-export async function getBalancerData(): Promise<FeeData> {
-  const _1daysAgo = Math.floor(Date.now() / 1000);
-  const _2daysAgo = Math.floor(Date.now() / 1000) - 86400;
+function dayOfYear(): number {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+  const oneDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(diff / oneDay);
+  return day;
+}
 
-  const request = await fetch("https://api.thegraph.com/subgraphs/name/balancer-labs/balancer", {
+const AUG_17_FIRST_BLOCK = 10674231;
+const AUG_17_DAY = 230;
+const BLOCKS_PER_DAY = 6348;
+
+export async function getBalancerData(): Promise<FeeData> {
+  const todayBlock = AUG_17_FIRST_BLOCK + ((dayOfYear() - AUG_17_DAY) * BLOCKS_PER_DAY);
+  const yesterdayBlock = todayBlock - BLOCKS_PER_DAY;
+  const weekAgoBlock = todayBlock - (BLOCKS_PER_DAY * 7);
+
+  const request = await fetch("https://api.thegraph.com/subgraphs/name/bonustrack/balancer", {
     headers: {
       "content-type": "application/json",
     },
     body: JSON.stringify({
       query: `{
-        pools (first: 1000, skip: 0, where:{ publicSwap: true },orderBy: liquidity, orderDirection: desc) {
-          id
-          swapFee
-          oneDay: swaps (first: 1,orderBy: timestamp, orderDirection: desc, where: {timestamp_lt: ${_1daysAgo}}) {
-            poolTotalSwapVolume
-          }
-          twoDays: swaps (first: 1,orderBy: timestamp, orderDirection: desc, where: {timestamp_lt: ${_2daysAgo}}) {
-            poolTotalSwapVolume
-          }
+        now: balancer(id: "1", block: {number: ${todayBlock}}) {
+          totalSwapFee
+        }
+        yesterday: balancer(id: "1", block: {number: ${yesterdayBlock}}) {
+          totalSwapFee
+        }
+        weekAgo: balancer(id: "1", block: {number: ${weekAgoBlock}}) {
+          totalSwapFee
         }
       }`,
-      variables: null
+      variables: null,
     }),
-    "method": "POST",
+    method: "POST",
   });
+
   const { data } = await request.json();
-
-  const oneDay = data.pools
-    .filter((pool: any) => pool.twoDays.length > 0 && pool.oneDay.length)
-    .map((pool: any) => ({
-      previousVolume: parseFloat(pool.twoDays[0].poolTotalSwapVolume),
-      currentVolume: parseFloat(pool.oneDay[0].poolTotalSwapVolume),
-      fee: parseFloat(pool.swapFee),
-    }))
-    .map((pool: any) => (pool.currentVolume - pool.previousVolume) * pool.fee)
-    .reduce((a: number, b: number) => a + b, 0);
-
-  // const sevenDayMA = data.uniswapDayDatas.reduce((total: number, { dailyVolumeInUSD }: any) => total + parseFloat(dailyVolumeInUSD), 0) * 0.003 / data.uniswapDayDatas.length;
 
   return {
     id: 'balancer',
     category: 'app',
-    sevenDayMA: 0,
-    oneDay,
+    sevenDayMA: (parseFloat(data.now.totalSwapFee) - parseFloat(data.weekAgo.totalSwapFee)) / 7,
+    oneDay: parseFloat(data.now.totalSwapFee) - parseFloat(data.yesterday.totalSwapFee),
   };
 }
