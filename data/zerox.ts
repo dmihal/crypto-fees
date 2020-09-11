11f5616bc1d55e950a1323f38bc2f3870bd6e0cf
David Mihal <david@dmihal.com>
2020-09-11 09:09:47 +0100

    Fix 0x

--- data/zerox.ts
+++ data/zerox.ts
@@ -2,14 +2,14 @@ import { FeeData } from './feeData';
 
 export async function get0xData(): Promise<FeeData> {
 
-  const request = await fetch("https://api.0xtracker.com/metrics/network?period=week&granularity=day");
+  const request = await fetch("https://api.0xtracker.com/metrics/network?period=month&granularity=day");
 
   const data = await request.json();
 
   return {
     id: 'zerox',
     category: 'app',
-    sevenDayMA: data.reduce((i: number, day: any) => i + day.protocolFees.USD, 0) / 7,
-    oneDay: data[6].protocolFees.USD,
+    sevenDayMA: data.slice(-8, -1).reduce((i: number, day: any) => i + day.protocolFees.USD, 0) / 7,
+    oneDay: data[data.length - 2].protocolFees.USD,
   };
 }
