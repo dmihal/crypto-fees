db5cd31bf046ab008c98f2acc6063f48140e2d53
David Mihal <david@dmihal.com>
2020-09-18 09:13:11 +0100

    Use rotating screenshot keys

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -4,7 +4,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function getScreenshot() {
-  const request = await fetch(`https://api.browshot.com/api/v1/screenshot/create?url=https://cryptofees.info/&instance_id=12&size=screen&cache=${60 * 60 * 24}&key=${process.env.NEXT_APP_BROWSHOT}`)
+  const apiKeys = process.env.NEXT_APP_BROWSHOT.split(',');
+  const key = apiKeys[(new Date()).getDate() % apiKeys.length];
+
+  const request = await fetch(`https://api.browshot.com/api/v1/screenshot/create?url=https://cryptofees.info/&instance_id=12&size=screen&cache=${60 * 60 * 24}&key=${key}`)
   const json = await request.json();
 
   if (json.status === 'in_queue') {
