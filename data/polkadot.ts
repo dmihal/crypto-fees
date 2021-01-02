b768a7dc29b159b86979ae04fcd01756487c93e1
David Mihal <david@dmihal.com>
2021-01-02 10:44:54 +0000

    Fix Polkadot calculations

--- data/polkadot.ts
+++ data/polkadot.ts
@@ -5,50 +5,46 @@ import format from 'date-fns/format';
 const startOfTodayUTC = () => new Date(new Date().setUTCHours(0, 0, 0, 0));
 
 export function getPolkadotData(): Promise<FeeData> {
-  return getSubstrateData(
-    'https://polkadot.subscan.io/api/scan/daily',
-    'polkadot',
-    'Polkadot',
-    10000000
-  );
+  return getSubstrateData('polkadot', 'Polkadot', 10 ** 10);
 }
 
 export function getKusamaData(): Promise<FeeData> {
-  return getSubstrateData(
-    'https://kusama.subscan.io/api/scan/daily',
-    'kusama',
-    'Kusama',
-    1000000000
-  );
+  return getSubstrateData('kusama', 'Kusama', 10 ** 12);
 }
 
-async function getSubstrateData(
-  url: string,
-  id: string,
-  name: string,
-  divisor: number
-): Promise<FeeData> {
-  const today = startOfTodayUTC();
-  const yesterday = subDays(today, 1);
-  const weekAgo = subDays(today, 7);
-
+async function fetchJSON(url: string, data: any) {
   const request = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
     },
-    body: JSON.stringify({
+    body: JSON.stringify(data),
+  });
+  const json = await request.json();
+  return json;
+}
+
+async function getSubstrateData(id: string, name: string, divisor: number): Promise<FeeData> {
+  const today = startOfTodayUTC();
+  const yesterday = subDays(today, 1);
+  const weekAgo = subDays(today, 7);
+
+  const [fees, prices] = await Promise.all([
+    fetchJSON(`https://${id}.subscan.io/api/scan/daily`, {
       start: format(weekAgo, 'yyyy-MM-dd'),
       end: format(yesterday, 'yyyy-MM-dd'),
       format: 'day',
       category: 'Fee',
     }),
-  });
+    fetchJSON(`https://${id}.subscan.io/api/scan/price/history`, {
+      start: format(weekAgo, 'yyyy-MM-dd'),
+      end: format(today, 'yyyy-MM-dd'),
+    }),
+  ]);
 
-  const { data } = await request.json();
-
-  const weekTotal = data.list.reduce(
-    (total: number, day: any) => total + parseInt(day.balance_amount_total),
+  const weekTotal = fees.data.list.reduce(
+    (total: number, day: any, i: number) =>
+      total + day.balance_amount_total * prices.data.list[i].price,
     0
   );
 
@@ -57,6 +53,6 @@ async function getSubstrateData(
     name,
     category: 'l1',
     sevenDayMA: weekTotal / divisor / 7,
-    oneDay: parseFloat(data.list[6].balance_amount_total) / divisor,
+    oneDay: (fees.data.list[6].balance_amount_total * prices.data.list[6].price) / divisor,
   };
 }
