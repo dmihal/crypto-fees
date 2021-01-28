8081dc8cd3edd38db74ba11cd8837b2fa7ddd7f5
sakulstra <lukasstrassel@googlemail.com>
2021-01-28 13:06:26 +0100

    fix: adjust to pol interface

--- data/aave.ts
+++ data/aave.ts
@@ -1,10 +1,16 @@
 import { FeeData } from './feeData';
 
+const fetcher = async (input: RequestInfo, init?: RequestInit) => {
+  const res = await fetch(input, init);
+  return res.json();
+};
+
 export async function getAaveData(): Promise<FeeData> {
+  const response = await fetcher('http://localhost:3000/data/fees');
   return {
     id: 'aave',
     category: 'app',
-    sevenDayMA: 0,
-    oneDay: 0,
+    sevenDayMA: parseFloat(response.last7DaysAvg),
+    oneDay: parseFloat(response.last24hFees),
   };
 }
