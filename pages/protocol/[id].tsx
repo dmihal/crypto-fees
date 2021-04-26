b14f7669cb9fa8513d8b33f4280082f88d43002e
David Mihal <david@dmihal.com>
2021-04-26 21:52:40 +0100

    Change date on charts

--- pages/protocol/[id].tsx
+++ pages/protocol/[id].tsx
@@ -1,15 +1,16 @@
 import { useState, useRef, useEffect } from 'react';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import { getIDs, getMetadata } from 'data/adapters';
-import { getDateRangeData } from 'data/queries';
+import DatePicker from 'react-datepicker';
 import addDays from 'date-fns/addDays';
 import subDays from 'date-fns/subDays';
-import isAfter from 'date-fns/isAfter'
+import isAfter from 'date-fns/isAfter';
 import Chart from 'components/Chart';
+import { getIDs, getMetadata } from 'data/adapters';
+import { getDateRangeData } from 'data/queries';
 import { formatDate } from 'data/lib/time';
 
 function getMissing(data: any, minDate: string, maxDate: string, id: string) {
-  const missing = []
+  const missing = [];
   if (!data[id]) {
     data[id] = {};
   }
@@ -23,7 +24,13 @@ function getMissing(data: any, minDate: string, maxDate: string, id: string) {
   return missing;
 }
 
-function formatData(data: any, minDate: string, maxDate: string, primary: string, secondary?: string | null) {
+function formatData(
+  data: any,
+  minDate: string,
+  maxDate: string,
+  primary: string,
+  secondary?: string | null
+) {
   const result = [];
   for (let date = new Date(minDate); !isAfter(date, new Date(maxDate)); date = addDays(date, 1)) {
     const dateStr = formatDate(date);
@@ -41,7 +48,7 @@ function saveFeeData(response: any, storedFees: any) {
     if (!storedFees[protocol.id]) {
       storedFees[protocol.id] = {};
     }
-    
+
     for (const { date, ...data } of protocol.data) {
       storedFees[protocol.id][date] = data;
     }
@@ -67,16 +74,17 @@ const useFees = (
     const missingSecondary = secondary ? getMissing(fees.current, minDate, maxDate, secondary) : [];
 
     if (missingPrimary.length > 0 || missingSecondary.length > 0) {
-      setValue(({ data }) => ({ data, loading: true }))
+      setValue(({ data }) => ({ data, loading: true }));
 
-      const secondaryQuery = missingSecondary.length > 0 ? `&${secondary}=${missingSecondary.join(',')}` : '';
+      const secondaryQuery =
+        missingSecondary.length > 0 ? `&${secondary}=${missingSecondary.join(',')}` : '';
       fetch(`/api/v1/feesByDay?${primary}=${missingPrimary.join(',')}&${secondaryQuery}`)
         .then((response: any) => response.json())
         .then((response: any) => {
           if (!response.success) {
             console.error(response);
             setValue(({ data }) => ({ data, loading: false }));
-            return
+            return;
           }
 
           saveFeeData(response.data, fees.current);
@@ -92,11 +100,10 @@ const useFees = (
         data: formatData(fees.current, minDate, maxDate, primary, secondary),
       });
     }
+  }, [minDate, maxDate, primary, secondary]);
 
-  }, [minDate, maxDate, primary, secondary])
-
-  return value
-}
+  return value;
+};
 
 interface ProtocolDetailsProps {
   id: string;
@@ -105,7 +112,12 @@ interface ProtocolDetailsProps {
   protocols: string[];
 }
 
-export const ProtocolDetails: NextPage<ProtocolDetailsProps> = ({ id, metadata, feeCache, protocols }) => {
+export const ProtocolDetails: NextPage<ProtocolDetailsProps> = ({
+  id,
+  metadata,
+  feeCache,
+  protocols,
+}) => {
   // const filteredData = feeCache[id].map((day: any) => ({
   //   date: new Date(day.date).getTime() / 1000,
   //   fee: day.fee,
@@ -121,20 +133,32 @@ export const ProtocolDetails: NextPage<ProtocolDetailsProps> = ({ id, metadata, 
       <h1 className="title">CryptoFees.info</h1>
       <h2 className="subtitle">{metadata.name}</h2>
 
-      <Chart
-        data={data}
-        loading={loading}
-        primary={id}
-        secondary={secondary}
-      />
+      <Chart data={data} loading={loading} primary={id} secondary={secondary} />
 
-      <select
-        value={secondary || 'None'}
-        onChange={(e: any) => setSecondary(e.target.value === 'None' ? null : e.target.value)}
-      >
-        <option>None</option>
-        {protocols.map((id: string) => <option key={id}>{id}</option>)}
-      </select>
+      <div>
+        <select
+          value={secondary || 'None'}
+          onChange={(e: any) => setSecondary(e.target.value === 'None' ? null : e.target.value)}
+        >
+          <option>None</option>
+          {protocols.map((id: string) => (
+            <option key={id}>{id}</option>
+          ))}
+        </select>
+
+        <DatePicker
+          selected={new Date(minDate)}
+          onChange={(newDate: any) => setMinDate(formatDate(newDate))}
+          maxDate={subDays(new Date(), 1)}
+          popperPlacement="bottom-end"
+        />
+        <DatePicker
+          selected={new Date(maxDate)}
+          onChange={(newDate: any) => setMaxDate(formatDate(newDate))}
+          maxDate={subDays(new Date(), 1)}
+          popperPlacement="bottom-end"
+        />
+      </div>
 
       <p>{metadata.description}</p>
       <p>{metadata.feeDescription}</p>
@@ -146,7 +170,11 @@ export default ProtocolDetails;
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const id = params.id.toString();
-  const defaultFeesArray = await getDateRangeData(id, subDays(new Date(), 90), subDays(new Date(), 1));
+  const defaultFeesArray = await getDateRangeData(
+    id,
+    subDays(new Date(), 90),
+    subDays(new Date(), 1)
+  );
   const defaultFees: { [date: string]: any } = {};
   for (const { date, ...data } of defaultFeesArray) {
     defaultFees[date] = data;
