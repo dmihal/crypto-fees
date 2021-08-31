b7e8250832f1ca0c8e0f908111e21efa12c4a721
sakulstra <lukasstrassel@googlemail.com>
2021-08-31 12:53:48 +0200

    fix: use os api

--- data/adapters/aave.ts
+++ data/adapters/aave.ts
@@ -1,43 +1,57 @@
+import { dateToTimestamp } from 'data/lib/time';
 import { Category, RegisterFunction } from 'data/types';
 import icon from 'icons/aave.svg';
 
-const fetcher = async (date: string, version: string) => {
-  const res = await fetch('https://aave-api-v2.aave.com/data/daily-fees', {
-    method: 'GET',
-    body: JSON.stringify({ date, version }),
+const fetcher = async (date: string, poolId: string) => {
+  const res = await fetch('http://localhost:3001/api/fees/queries/getFees', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ params: { timestamp: dateToTimestamp(date), poolId } }),
   });
   if (res.status !== 200) throw new Error('aave did return an error');
-  return res.json();
+  return (await res.json()).result;
 };
 
-async function getAaveV1Data(date: string): Promise<number> {
-  const response = await fetcher(date, 'v1');
+async function getAaveV1ProtoData(date: string): Promise<number> {
+  const response = await fetcher(date, '0x24a42fd28c976a61df5d00d0599c34c4f90748c8');
 
   if (response.error) {
     throw new Error(response.error);
   }
-
-  return parseFloat(response?.lastDayUTCFees);
+  return parseFloat(response?.last24hFees);
 }
 
-async function getAaveV2Data(date: string): Promise<number> {
-  const response = await fetcher(date, 'v2');
+async function getAaveV2ProtoData(date: string): Promise<number> {
+  const response = await fetcher(date, '0xb53c1a33016b2dc2ff3653530bff1848a515c8c5');
 
   if (response.error) {
     throw new Error(response.error);
   }
 
-  return parseFloat(response?.lastDayUTCFees);
+  return parseFloat(response?.last24hFees);
+}
+
+async function getAaveV2AmmData(date: string): Promise<number> {
+  const response = await fetcher(date, '0xacc030ef66f9dfeae9cbb0cd1b25654b82cfa8d5');
+
+  if (response.error) {
+    throw new Error(response.error);
+  }
+
+  return parseFloat(response?.last24hFees);
 }
 
 async function getAavePolygonData(date: string): Promise<number> {
-  const response = await fetcher(date, 'polygon');
+  const response = await fetcher(date, '0xd05e3e715d945b59290df0ae8ef85c1bdb684744');
 
   if (response.error) {
     throw new Error(response.error);
   }
 
-  return parseFloat(response?.lastDayUTCFees);
+  return parseFloat(response?.last24hFees);
 }
 
 export default function registerAave(register: RegisterFunction) {
@@ -46,7 +60,7 @@ export default function registerAave(register: RegisterFunction) {
     date: string
   ) => {
     if (attribute !== 'fee') {
-      throw new Error(`Uniswap doesn't support ${attribute}`);
+      throw new Error(`Aave doesn't support ${attribute}`);
     }
     return adapter(date);
   };
@@ -56,7 +70,8 @@ export default function registerAave(register: RegisterFunction) {
     bundle: 'aave',
     name: 'Aave',
     category: 'lending' as Category,
-    description: 'Aave is an open borrowing & lending protocol.',
+    description:
+      'Aave is an open source and non-custodial liquidity protocol for earning interest on deposits and borrowing assets.',
     feeDescription: 'Interest fees are paid from borrowers to lenders.',
     blockchain: 'Ethereum',
     source: 'Aave',
@@ -68,17 +83,22 @@ export default function registerAave(register: RegisterFunction) {
     protocolLaunch: '2020-01-08',
   };
 
-  register('aave-v1', query(getAaveV1Data), {
+  register('aave-v1', query(getAaveV1ProtoData), {
     ...aaveMetadata,
     subtitle: 'Aave V1',
     protocolLaunch: '2020-01-08',
   });
-  register('aave-v2', query(getAaveV2Data), {
+  register('aave-v2', query(getAaveV2ProtoData), {
     ...aaveMetadata,
     subtitle: 'Aave V2',
     protocolLaunch: '2020-12-03',
   });
-  register('aave-polygon', query(getAavePolygonData), {
+  register('aave-v2-amm', query(getAaveV2AmmData), {
+    ...aaveMetadata,
+    subtitle: 'Aave V2 Amm',
+    protocolLaunch: '2021-03-08',
+  });
+  register('aave-v2-polygon-proto', query(getAavePolygonData), {
     ...aaveMetadata,
     subtitle: 'Aave V2 Polygon',
     protocolLaunch: '2021-03-31',
