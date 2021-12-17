8805f786c77376836435b1469970cc45041ccbd5
David Mihal <david@dmihal.com>
2021-12-17 19:05:41 +0000

    Move a lot of adapters to CryptoStats

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,17 +2,11 @@ import bancor from './bancor.png';
 import curve from './curve.png';
 import hegic from './hegic.png';
 import honeyswap from './honeyswap.png';
-import kusama from './kusama.svg';
-import maker from './maker.svg';
 import mstable from './mstable.svg';
 import omen from './omen.png';
-import polkadot from './polkadot.svg';
 import polygon from './polygon.svg';
 import polymarket from './polymarket.svg';
-import quickswap from './quickswap.png';
 import tbtc from './tbtc.svg';
-import terra from './terra.svg';
-import tornado from './tornado.svg';
 import linkswap from './linkswap.svg';
 import zerox from './zrx.svg';
 import zilliqa from './zilliqa.svg';
@@ -23,17 +17,11 @@ const icons: { [id: string]: string } = {
   curve,
   hegic,
   honeyswap,
-  kusama,
-  maker,
   mstable,
   omen,
-  polkadot,
   polygon,
   polymarket,
-  quickswap,
   tbtc,
-  terra,
-  tornado,
   linkswap,
   zerox,
   zilliqa,
