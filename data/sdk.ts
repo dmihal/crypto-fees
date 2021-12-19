6956a11e7820c459799ba76bef8ffc0a153021d9
David Mihal <david@dmihal.com>
2021-12-19 14:18:27 +0000

    Allow 1s to initialize adapter

--- data/sdk.ts
+++ data/sdk.ts
@@ -4,6 +4,7 @@ const sdk = new CryptoStatsSDK({
   mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
   redisConnectionString: process.env.REDIS_URL,
   ipfsGateway: 'https://ipfs.cryptostats.community',
+  executionTimeout: 1000,
   adapterListSubgraph: 'dmihal/cryptostats-adapter-registry-test',
 });
 
