c9dab8f623fb08e59e445482fb1b0a5d785c3e9f
David Mihal <david@dmihal.com>
2022-03-23 21:17:32 +0200

    Disable i18n debug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,8 +17,6 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: i18nReources as any,
-    // lng: "en",
-    debug: true,
     detection: {
       caches: [],
     },
