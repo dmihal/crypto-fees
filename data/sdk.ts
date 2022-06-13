904ef83eccfba5435e8f8c3f73aad3f7e6ad10a3
David Mihal <david@dmihal.com>
2022-06-13 22:17:33 +0200

    Use POKT for RPC

--- data/sdk.ts
+++ data/sdk.ts
@@ -3,14 +3,18 @@ import { CryptoStatsSDK } from '@cryptostats/sdk';
 const sdk = new CryptoStatsSDK({
   mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
   redisConnectionString: process.env.REDIS_URL,
-  ipfsGateway: 'https://ipfs.cryptostats.community',
   executionTimeout: 1000,
-  adapterListSubgraph: 'dmihal/cryptostats-adapter-registry-test',
-  moralisKey: process.env.MORALIS_KEY,
 });
 
 if (process.env.NEXT_PUBLIC_OPTIMISM_RPC) {
-  sdk.ethers.addProvider('optimism', process.env.NEXT_PUBLIC_OPTIMISM_RPC, { archive: true })
+  sdk.ethers.addProvider('optimism', process.env.NEXT_PUBLIC_OPTIMISM_RPC, { archive: true });
+}
+if (process.env.POKT_KEY) {
+  sdk.ethers.addProvider(
+    'ethereum',
+    `https://eth-archival.gateway.pokt.network/v1/lb/${process.env.POKT_KEY}`,
+    { archive: true }
+  );
 }
 
 export default sdk;
